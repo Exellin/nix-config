19648c2e5a8a3fac756764281c81d8aa2c141731
Shawn Carnegie <carnegieshawn@gmail.com>
2024-05-29 21:14:41 -0600

    Create flake.nix file and use it for rebuilding nix from inside the lockfiles directory
    Following the tutorial at https://www.youtube.com/watch?v=ACybVzRvDhs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-My dotfiles
+## Commands
+
+- Grab latest versions of packages to update flake.lock: `nix flake update`
+- Rebuild NixOs from within dotfiles: `sudo nixos-rebuild switch --flake .`
--- configuration.nix
+++ configuration.nix
@@ -118,4 +118,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }

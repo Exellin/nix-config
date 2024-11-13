64e177f8d5187fb471a81031777921b5abb1fba3
Shawn Carnegie <carnegieshawn@gmail.com>
2024-11-12 21:50:52 -0700

    Use nixos-unstable in order to get deno v2 working
    - Following the document at https://github.com/mcdonc/.nixconfig/blob/master/videos/replacingoverlays/script.rst

--- flake.nix
+++ flake.nix
@@ -3,26 +3,36 @@
 
  inputs = {
   nixpkgs.url = "nixpkgs/nixos-24.05";
+  nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   home-manager.url = "github:nix-community/home-manager/release-24.05";
   home-manager.inputs.nixpkgs.follows = "nixpkgs";
  };
 
- outputs = { self, nixpkgs, home-manager, ... }:
+ outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    specialArgs = {
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      inherit system;
+      inherit inputs;
+    };
   in {
    nixosConfigurations = {
      nixos = lib.nixosSystem {
       inherit system;
-      modules = [ ./configuration.nix ];   
+      inherit specialArgs;
+      modules = [ ./configuration.nix ];
      };
    };
    homeConfigurations = {
     shawnc = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
-      modules = [ ./home.nix ];   
+      modules = [ ./home.nix ];
      };
    };
   };

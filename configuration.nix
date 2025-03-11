997d8d1180e5a77667518dd6aec136050bbaed48
Shawn Carnegie <carnegieshawn@gmail.com>
2025-03-11 12:39:20 -0600

    Update to nixos 24.11
    Release Notes: https://github.com/NixOS/nixpkgs/blob/3eeff54780a1a8c73c82ca51987962b62bd4219e/nixos/doc/manual/release-notes/rl-2411.section.md
    
    microsoft-edge-dev seems to have been removed

--- configuration.nix
+++ configuration.nix
@@ -47,8 +47,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -77,7 +75,6 @@
       kate
       git
       microsoft-edge
-      microsoft-edge-dev
       discord
       insync
       vscode
